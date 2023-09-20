8f67fa0159b94690d08b2cae4198729c2b31104c
elizerdim <elizerdim@gmail.com>
2023-09-20 21:49:15 +0300

    Fix class names acc. to BEM convention

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const messages = document.getElementsByClassName('msg');
+const messages = document.getElementsByClassName('phone__msg');
 
 for (let i = 0; messages.length > i; i++) {
   setTimeout(() => messages[i].classList.add('display'), i * 1000);
